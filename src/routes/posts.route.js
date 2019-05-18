011fb2ff3234affacf8a37d7cc040c7b5466a264
JeongDawon <fors8@ajou.ac.kr>
2019-05-18 14:39:16 +0900

    change boards --> posts

---
+++ src/routes/posts.route.js
@@ -0,0 +1,18 @@
+import { Router } from 'express'
+import { add, index } from '../controllers/posts.controller'
+import { connection } from '../models/connection'
+
+const router = Router()
+
+router.post('/', add)
+
+router.get('/', index)
+
+// router.get('/qna')
+// router.get('/:id', indsssex)
+
+// const index = (req, res, next) => {
+//     let qndId = req.params.id
+// }
+
+export default router
