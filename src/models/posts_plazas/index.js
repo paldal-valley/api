45aa79b4b3e629a3fea193d8fc153dfcb56b0bf4
makehoney <makehoney3@gmail.com>
2019-05-18 22:18:24 +0900

    [Edit DAO] sql 수정

--- src/models/posts_plazas/index.js
+++ src/models/posts_plazas/index.js
@@ -1,20 +1,29 @@
 import con from '@db'
 
-const getList = () => {
+const getList = (options = {}) => {
   return new Promise((resolve, reject) => {
+    const { limit = 5 } = options
+    const injection = [Number(limit)]
+
     const sql = `
     SELECT
+      p.id,
       p.title,
+      p.content,
       p.view,
       p.recommended
     FROM
-      posts_plazas pp  
+      posts_plazas pp
     LEFT JOIN
       posts p
     ON
       p.id = pp.postId
+    
+    ORDER BY p.createdDate DESC
+    
+    LIMIT ?
     `
-    con.query(sql, (err, result) => {
+    con.query(sql, injection ,(err, result) => {
       if (err) return reject(err)
 
       return resolve(result)
