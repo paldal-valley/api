132ebd1fb998326661e374d6ff56a151a260cd8a
makehoney <makehoney3@gmail.com>
2019-05-18 16:59:25 +0900

    [Add routes] plaza 테스트 라우터 추가

--- src/routes/posts.route.js
+++ src/routes/posts.route.js
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import { add } from '../controllers/boards.controller'
+import { getPostPlazaList } from '../controllers/posts.controller'
 const router = Router()
 
 router.post('/', add)
+router.get('/plaza', getPostPlazaList)
 
 export default router
 
