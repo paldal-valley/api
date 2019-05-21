66c5853544a93a177968ae8ca87844dd507fdb8a
makehoney <makehoney3@gmail.com>
2019-05-21 18:04:16 +0900

    [Edit app] 전역 미들웨어 me 추가 및 코드 리팩토링

--- src/app.js
+++ src/app.js
@@ -4,22 +4,21 @@ import cors from 'cors'
 import 'dotenv/config'
 import './utils/module-alias'
 
-import api from './routes'
 import errorHandler from '@middle/error'
+import me from '@middle/me'
+import api from './routes'
 
 const app = express()
-app.set('port', process.env.PORT || 8000)
 
 app.use(express.json())
 app.use(morgan('dev'))
 app.use(cors())
+app.use(me)
 app.use('/', api)
 app.use(errorHandler)
 
-
-
-const server = app.listen(app.get('port'), () => {
-  console.log(`Server is running on port ${app.get('port')}`)
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`)
 })
 
 // For API test
