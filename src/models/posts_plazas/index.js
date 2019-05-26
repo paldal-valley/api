f849d7ed260cc937516accd7d88acaa8844559d6
makehoney <makehoney3@gmail.com>
2019-05-26 15:35:32 +0900

    [Edit SQL] 잘못된 쿼리 수정

--- src/models/posts_plazas/index.js
+++ src/models/posts_plazas/index.js
@@ -119,7 +119,7 @@ const updateOne = (id = 0, payload = {}) => {
     SET
       ?
     WHERE
-      id = ?
+      postId = ?
     `
     con.query(sql, injection, (err, result) => {
       if (err) return reject(err)
