fba2ff6e17fdbe611ea3727ad7960edc61b2c25d
chiheon <qwaserdf234@naver.com>
2019-06-03 01:22:04 +0900

    [User Password Change Add] resetToken과 resetToken 발행 후 시간을 체크하여 비밀번호 변경하는 로직 추가

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,9 +1,10 @@
 import { Router } from 'express'
-import { addUser } from '../controllers/user.controller'
+import { addUser, resetUserPassword } from '../controllers/user.controller'
 import { emailDupCheck } from '@middle/auth'
 
 const router = Router()
 
 router.post('/', emailDupCheck, addUser)
+router.post('/reset/:token', resetUserPassword)
 
 export default router
