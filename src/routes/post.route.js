907772b3da4ba222e2319555e8fa54f8fe8b2869
makehoney <makehoney3@gmail.com>
2019-06-10 17:28:30 +0900

    [CI TEST]

--- src/routes/post.route.js
+++ src/routes/post.route.js
@@ -37,8 +37,8 @@ import { isLoggedIn } from '@middle/auth'
 const router = Router()
 
 // plaza
-router.get('/plaza', getPostPlazaList)
-router.get('/plaza/:postId', getPostPlaza)
+router.get('/plaza', isLoggedIn, getPostPlazaList)
+router.get('/plaza/:postId', isLoggedIn, getPostPlaza)
 router.post('/plaza', isLoggedIn, addPostPlaza)
 router.put('/plaza/:postId', isLoggedIn, hasPost, updatePostPlaza)
 
