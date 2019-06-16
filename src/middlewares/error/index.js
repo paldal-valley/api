e46c1c7a9c61aef390b04419bb26e1b42c0e9b49
makehoney <makehoney3@gmail.com>
2019-06-16 20:12:44 +0900

    [Refactor Middleware] 에러 미들웨어 리팩토링

--- src/middlewares/error/index.js
+++ src/middlewares/error/index.js
@@ -1,24 +1,14 @@
-const Slack = require('slack-node');
-
-const webhookUri = "https://hooks.slack.com/services/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";
-
-const slack = new Slack()
-slack.setWebhook(webhookUri)
-const send = async(message) => {
-  slack.webhook({
-    channel: "#error_monitoring", // 전송될 슬랙 채널
-    username: "webhookbot", //슬랙에 표시될 이름
-    text: message
-  }, function(err, response) {
-    console.log(response);
-  });
-}
+import slackWebhook from '@utils/slack-webhook'
 
 export default (err, req, res, next) => {
-  if (process.env.NODE_ENV === 'production') {
-    send(err.message)
 
-    console.log('Middleware Error22')
+  if (process.env.NODE_ENV === 'production') {
+    console.error(`Controller Error: ${err}`)
+
+    slackWebhook({
+      channel: '#error_monitoring',
+      text: err.message
+    })
   } else {
     console.error(err)
   }
